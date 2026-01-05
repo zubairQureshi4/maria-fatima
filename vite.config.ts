4ef5071473e1f0bdd8f252ab9848a550ceb9566e
Zubair Qureshi <94286111+zubairQureshi4@users.noreply.github.com>
2026-01-05 20:20:42 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/maria-fatima/",
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
